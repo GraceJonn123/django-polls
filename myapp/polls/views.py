52a55084bc40447caa9a94d3b342e607fd46e018
GraceJonn123 <gj.gjonn@gmail.com>
2016-02-13 01:19:57 +0300

    all migrations for polls

--- myapp/polls/views.py
+++ myapp/polls/views.py
@@ -3,3 +3,15 @@ from django.shortcuts import HttpResponse
 # Create your views here.
 def index(request):
 	return HttpResponse("Hello, world. You're at polls index.")
+from django.db import models
+
+
+class Question(models.Model):
+    question_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
+
+
+class Choice(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
